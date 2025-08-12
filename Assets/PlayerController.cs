eee63a61a0c42d9fc947a5a4902811237bd263ea
octavioantich <62580250+octavioantich@users.noreply.github.com>
2025-08-11 23:52:06 -0300

    Dash added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,10 +4,21 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movimiento")]
     public float moveSpeed = 5f;
 
+    [Header("Dash")]
+    public float dashSpeed = 15f;
+    public float dashDuration = 0.2f;
+    public float dashCooldown = 1f; 
+    public Transform dashTargetOrbital;
+    
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private bool isDashing = false;
+    private float dashTimer = 0f;
+    private float dashCooldownTimer= 0f;
+    private Vector2 dashDirection;
 
     private void Awake()
     {
@@ -16,17 +27,47 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        moveInput = Vector2.zero;
-        if (Keyboard.current.wKey.isPressed) moveInput.y += 1f;
-        if (Keyboard.current.sKey.isPressed) moveInput.y -= 1f;
-        if (Keyboard.current.dKey.isPressed) moveInput.x += 1f;
-        if (Keyboard.current.aKey.isPressed) moveInput.x -= 1f;
+        if (dashCooldownTimer > 0f)
+            dashCooldownTimer -= Time.deltaTime;
 
-        moveInput = moveInput.normalized;
+        if (!isDashing)
+        {
+            moveInput = Vector2.zero;
+            if (Keyboard.current.wKey.isPressed) moveInput.y += 1f;
+            if (Keyboard.current.sKey.isPressed) moveInput.y -= 1f;
+            if (Keyboard.current.dKey.isPressed) moveInput.x += 1f;
+            if (Keyboard.current.aKey.isPressed) moveInput.x -= 1f;
+
+            moveInput = moveInput.normalized;
+        }
+
+        if (Keyboard.current.leftShiftKey.wasPressedThisFrame && 
+            dashTargetOrbital != null && 
+            !isDashing && 
+            dashCooldownTimer <= 0f)
+        {
+            dashDirection = (dashTargetOrbital.position - transform.position).normalized;
+            isDashing = true;
+            dashTimer = dashDuration;
+            dashCooldownTimer = dashCooldown;
+        }
     }
-
     private void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        if (isDashing)
+        {
+            rb.linearVelocity = dashDirection * dashSpeed;
+            dashTimer -= Time.fixedDeltaTime;
+
+            if (dashTimer <= 0f)
+            {
+                isDashing = false;
+            }
+        }
+        else
+        {
+            rb.linearVelocity = moveInput * moveSpeed;
+        }
+    
     }
 }
