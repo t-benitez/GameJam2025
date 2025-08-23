34375a527a146ce479377499ba2418719f19155c
Kushi <68166561+kushiros@users.noreply.github.com>
2025-08-23 04:16:56 -0300

    Added bullet animation

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -5,9 +5,14 @@ public class EnemyProjectile : MonoBehaviour
     public float damage = 1f;
     public float lifetime = 5f;
 
+    [Header("Animator")]
+    [SerializeField] private Animator animator;
+
     private void Awake()
     {
         Destroy(gameObject, lifetime);
+        StartCoroutine(projectile());
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,7 +21,15 @@ public class EnemyProjectile : MonoBehaviour
         if (player != null)
         {
             player.TakeDamage((int)damage);
-            Destroy(gameObject);
+            animator.SetTrigger("Hit");
+            Destroy(gameObject, 0.2f);
         }
     }
+
+    private System.Collections.IEnumerator projectile()
+    {
+        yield return new WaitForSeconds(lifetime-0.2f);
+        animator.SetTrigger("Hit");
+    }
+    
 }
